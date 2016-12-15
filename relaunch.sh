3c139036dbaa48f3e6b41ef78028ebb2b5e42578
Chris Hanaoka <chanaoka@polyverse.io>
2016-12-14 17:05:35 -0800

    split pvlib.js into appinfo.js and statter.js

--- relaunch.sh
+++ relaunch.sh
@@ -2,6 +2,9 @@
 
 docker rm -f -v polyverse_supervisor_1
 docker rm -f -v $(docker ps -qa)
+docker rm -f -v $(docker ps -qa)
+
+docker build -t polyverse/polysploit .
 
 docker run -d --name=polyverse_supervisor_1 -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/polyverse.yml:/polyverse.yml polyverse/supervisor:3e11e266c5d0c7aeed32f826da53eaece5f9411f -config-yaml-file=/polyverse.yml
 
