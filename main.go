311ec5a472c2241ffe6ed9bfc7a33f0b40e726a9
Archis Gore <archis@polyverse.io>
2018-08-31 10:11:20 -0700

    Allow ropoly address to be externalized, and use flags to provide help

--- main.go
+++ main.go
@@ -1,41 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-
-	"github.com/polyverse/ropoly/handlers"
+	"github.com/polyverse/ropoly/server"
+	"github.com/polyverse/ropoly/wiring"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	api := router.PathPrefix("/api/v0").Subrouter()
-	api.HandleFunc("/test", handlers.ROPTestHandler)
-	api.HandleFunc("/pids", handlers.ROPPIdsHandler)
-
-	pid := api.PathPrefix("/pid/{pid}").Subrouter()
-	pid.HandleFunc("/libraries", handlers.ROPLibrariesHandler)
-
-	mem := pid.PathPrefix("/memory").Subrouter()
-	mem.HandleFunc("/regions", handlers.ROPMemoryRegionsHandler)
-	mem.HandleFunc("/search", handlers.ROPMemorySearchHandler)
-	mem.HandleFunc("/disasm", handlers.ROPMemoryDisAsmHandler)
-	mem.HandleFunc("/gadget", handlers.ROPMemoryGadgetHandler)
-	mem.HandleFunc("/fingerprint", handlers.ROPMemoryFingerprintHandler)
-
-	// Dump the actual routes that the router knows about
-	router.Walk(
-		func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-			t, err := route.GetPathTemplate()
-			if err != nil {
-				return err
-			}
-			fmt.Println(t)
-			return nil
-		})
-
-	log.Fatal(http.ListenAndServe(":8008", router))
+	cfg := wiring.ParseFlags()
+	server.ServeOverHttp(cfg.HttpAddress)
 }
