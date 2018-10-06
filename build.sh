cae85c7ec9f342ad1095d3e286a8a8530c1d123e
Archis Gore <archis@polyverse.io>
2018-10-06 08:10:11 -0700

    Build libc (Everyone else) and musl (Alpine) ropoly

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 mkdir bin
+
 docker run --rm -it \
      -v $PWD:/go/src/github.com/polyverse/ropoly \
      -w /go/src/github.com/polyverse/ropoly \
-     golang 
+     golang scripts/buildlibc.sh
 
 echo "Copying libc binary into bin..."
 cp ./ropoly ./bin/ropoly-libc-x86_64
+
+docker run --rm -it \
+     -v $PWD:/go/src/github.com/polyverse/ropoly \
+     -w /go/src/github.com/polyverse/ropoly \
+     golang:alpine scripts/buildmusl.sh
+
+echo "Copying musl binary into bin..."
+cp ./ropoly ./bin/ropoly-musl-x86_64
