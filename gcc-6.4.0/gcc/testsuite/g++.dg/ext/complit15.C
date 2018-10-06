f1951cd822a2f3635b4a55945af484c25d369c3d
Archis Gore <archis@polyverse.io>
2018-10-05 19:08:39 -0700

    Remove binaries

--- gcc-6.4.0/gcc/testsuite/g++.dg/ext/complit15.C
+++
@@ -1,8 +0,0 @@
-// PR c++/79580
-// { dg-options "-flto -std=c++98" }
-
-class a
-{
-  static const double b;
-};
-const double a::b ((union { double c; }){}.c);
--- gcc-6.4.0/gcc/testsuite/gcc.dg/torture/pr79732.c
+++
@@ -1,5 +0,0 @@
-/* { dg-do link } */
-
-int bar () __attribute__ ((alias ("foo")));
-void foo () { }
-int main () { return bar(); }
