3ec28a223d7e8ebfdc7f015a6f240ced335d157d
Thomas Oldham <toldham@uw.edu>
2018-11-20 10:36:57 -0800

    Fixed EnVisen EQI.

--- lib/eqi/OriginalEnvisenEqi.go
+++ lib/eqi/OriginalEnvisenEqi.go
@@ -6,29 +6,65 @@ import (
 	"github.com/polyverse/ropoly/lib/types"
 )
 
-func OriginalEnvisenEqi(comparison types.FingerprintComparison, _ url.Values) (float64, error) {
-	gadgetCount := len(comparison.GadgetDisplacements)
-	weaklySurvivedCount := len(comparison.SingleDisplacements)
-	fractionSurvivedGadgets := float64(comparison.SurvivedGadgetCount) / float64(gadgetCount)
-	if weaklySurvivedCount == 0 {
-		return fractionSurvivedGadgets, nil
+func OriginalEnvisenEqi(c types.FingerprintComparison, _ url.Values) (float64, error) {
+	return strongSurvival(c) + (weakSurvivalOnly(c) * movementQuality(c)), nil
+}
+
+func strongSurvival(c types.FingerprintComparison) float64 {
+	return float64(c.SurvivedGadgetCount) / float64(len(c.GadgetDisplacements))
+}
+
+func weakSurvivalOnly(c types.FingerprintComparison) float64 {
+	return weakSurvival(c) - strongSurvival(c)
+}
+
+func weakSurvival(c types.FingerprintComparison) float64 {
+	return float64(len(c.SingleDisplacements)) / float64(len(c.GadgetDisplacements))
+}
+
+func movementQuality(c types.FingerprintComparison) float64 {
+	if len(c.GadgetsBySingleOffset) <= 1 {
+		return 0.0
 	}
+	return numberOfOffsets(c) * offsetStandardDeviation(c) / numberOfMovedGadgets(c) / highestOffsetCount(c)
+}
 
-	movementScale := float64(weaklySurvivedCount) / float64(gadgetCount)
+func numberOfOffsets(c types.FingerprintComparison) float64 {
+	return float64(len(c.GadgetsBySingleOffset))
+}
 
-	highestOffsetCount := 0
-	meanOffsetCount := float64(weaklySurvivedCount) / float64(len(comparison.GadgetsBySingleOffset))
-	totalSquareDeviation := float64(0)
-	for _, count := range comparison.GadgetsBySingleOffset {
-		if count > highestOffsetCount {
-			highestOffsetCount = count
+func offsetStandardDeviation(c types.FingerprintComparison) float64 {
+	mean := meanOffset(c)
+	totalSquareDeviation := 0.0
+	for _, count := range c.GadgetsBySingleOffset {
+		deviation := float64(count) - mean
+		totalSquareDeviation += deviation * deviation
+	}
+	return math.Sqrt(totalSquareDeviation / (float64(len(c.GadgetsBySingleOffset)) - 1))
+}
+
+func meanOffset(c types.FingerprintComparison) float64 {
+	return offsetTotal(c) / float64(len(c.GadgetsBySingleOffset))
+}
+
+func offsetTotal(c types.FingerprintComparison) float64 {
+	total := 0
+	for _, count := range c.GadgetsBySingleOffset {
+		total += count
+	}
+	return float64(total)
+}
+
+func numberOfMovedGadgets(c types.FingerprintComparison) float64 {
+	return float64(len(c.SingleDisplacements))
+}
+
+func highestOffsetCount(c types.FingerprintComparison) float64 {
+	highest := 0
+	for _, count := range c.GadgetsBySingleOffset {
+		if count > highest {
+			highest = count
 		}
-		deviation := float64(count) - meanOffsetCount
-		squareDeviation := deviation * deviation
-		totalSquareDeviation += squareDeviation
 	}
-	offsetStdev := math.Sqrt(totalSquareDeviation / float64(len(comparison.GadgetsBySingleOffset)))
-	movementPathologicalness := offsetStdev / float64(highestOffsetCount)
-
-	return fractionSurvivedGadgets + (movementScale * movementPathologicalness), nil
+	return float64(highest)
 }
