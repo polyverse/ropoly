189233154b1895b966c527f7ad21ad198969ce57
Thomas Oldham <toldham@uw.edu>
2019-01-22 10:57:20 -0800

    Added EQI tests to test script

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 test_pair() {
 	local input="$1"
 	local expected="$2"
@@ -10,9 +11,23 @@ test_pair() {
 	fi
 }
 
-test_pair "health" "\"Ropoly API Healthy\""
+# health
+test_pair health "\"Ropoly API Healthy\""
 
-curl -F "fingerprint=@TestFiles/fingerprint" localhost:8008/api/v1/fingerprints/uploadtest?overwrite=true
-test_pair "fingerprints/uploadtest" "$(cat TestFiles/fingerprint)"
+# fingerprint POST and GET
+curl -F fingerprint=@TestFiles/fingerprint localhost:8008/api/v1/fingerprints/uploadtest?overwrite=true
+test_pair fingerprints/uploadtest "$(cat TestFiles/fingerprint)"
+
+# eqi
+curl -F fingerprint=@TestFiles/original localhost:8008/api/v1/fingerprints/original?overwrite=true
+curl -F fingerprint=@TestFiles/aslr localhost:8008/api/v1/fingerprints/aslr?overwrite=true
+curl -F fingerprint=@TestFiles/eqi50 localhost:8008/api/v1/fingerprints/eqi50?overwrite=true
+curl -F fingerprint=@TestFiles/eqi90 localhost:8008/api/v1/fingerprints/eqi90?overwrite=true
+curl -F fingerprint=@TestFiles/allDead localhost:8008/api/v1/fingerprints/allDead?overwrite=true
+test_pair fingerprints/original/eqi?func=shared-offsets\&second=original 0
+test_pair fingerprints/original/eqi?func=shared-offsets\&second=aslr 0
+test_pair fingerprints/original/eqi?func=shared-offsets\&second=eqi50 50
+test_pair fingerprints/original/eqi?func=shared-offsets\&second=eqi90 90
+test_pair fingerprints/original/eqi?func=shared-offsets\&second=allDead 100
 
 echo "All tests passed"
